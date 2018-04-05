cd2ee7396a3b73f2947338da5bf877fc5e5fc3c2
Prashant Gaikwad <prashant.gaikwad@leftrightmind.com>
2018-04-05 22:06:45 +0530

    initial commit

--- Day 2 - IBDesignable Demo/ViewController.swift
+++ Day 2 - IBDesignable Demo/ViewController.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+/*
+ 
+ @IBDesignable available from iOS 8.0 and Xode 6 or later.  The @IBDesignable properties applied to the UIView or NSView subclass. and it render the view directly in StoryBoard, So that we can easily identify about the view before run the app.
+ 
+ */
+
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
